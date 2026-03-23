25fb79d555382c6550aa5c0ba129faaa8feda55e
kimsinjung189 <kimsinjung189@gamail.com>
2026-03-23 09:31:40 +0900

    feat: 테마 스타일 및 UI 적용

---
+++ src/styles/theme.js
@@ -0,0 +1,98 @@
+const theme = {
+  layout: {
+    maxWidth: "1440px",
+    padding: "0 20px",
+  },
+
+  fontSize: {
+    xs: "12px",
+    sm: "14px",
+    md: "16px",
+    lg: "18px",
+    xl: "20px",
+    xxl: "24px",
+    xxxl: "28px",
+    displaySm: "30px",
+    displayMd: "40px",
+    displayLg: "44px",
+    displayXl: "56px",
+    displayXxl: "60px",
+    displayXxxl: "68px",
+  },
+
+  colors: {
+    primary: "#8FA77E",
+    primaryDark: "#4F6A4E",
+    error: "#D74040",
+    sale: "#E53935",
+    freeDelivery: "#4A90E2",
+    orderBadge: "#6CA346",
+    textMain: "#222222",
+    textSub: "#7C7C7C",
+    border: "#E2E2E2",
+    borderFocus: "#DCDCDC",
+    bg: "#FFFFFF",
+    bgSoft: "#F9F9F9",
+    red: "#FF4D4F",
+    orange: "#FA8C16",
+    yellow: "#FADB14",
+    green: "#52C41A",
+    blue: "#1677FF",
+    navy: "#1D39C4",
+    purple: "#722ED1",
+
+    /* ========================= 단색 ( 필요 시 사용 )========================= */
+    white: "#FFFFFF",
+    black: "#000000",
+    gray100: "#F5F5F5",
+    gray200: "#E8E8E8",
+    gray300: "#D9D9D9",
+    gray400: "#BFBFBF",
+    gray500: "#8C8C8C",
+    gray600: "#595959",
+    gray700: "#434343",
+    gray800: "#262626",
+    gray900: "#1F1F1F",
+  },
+
+  badge: {
+    sale: "#FB8C00",
+    freeDelivery: "#3561E5",
+    best: "#E53935",
+    new: "#43A047",
+  },
+  border: {
+    thin: "1px solid #E2E2E2",
+    thick: "2px solid #DCDCDC",
+  },
+
+  radius: {
+    sm: "4px",
+    md: "6px",
+    lg: "8px",
+    xl: "10px",
+    xxl: "12px",
+    xxxl: "34px",
+
+    pill: "999px",
+  },
+
+  spacing: {
+    section: "120px",
+    title: "70px",
+  },
+
+  media: {
+    tablet: "@media (max-width: 1024px)",
+    mobile: "@media (max-width: 768px)",
+    smallMobile: "@media (max-width: 480px)",
+  },
+
+  shadow: {
+    sm: "0 2px 6px rgba(0,0,0,0.05)",
+    md: "0 4px 12px rgba(0,0,0,0.08)",
+    lg: "0 8px 20px rgba(0,0,0,0.12)",
+  },
+};
+
+export default theme;
