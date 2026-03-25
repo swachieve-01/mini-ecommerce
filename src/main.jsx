489c1f1eb581b602c65f74d75e23ed0c838e6c0c
Winter-Haeum <rudnfgkdma13@gmail.com>
2026-03-25 16:34:34 +0900

    chore: BrowserRouter, ThemeProvider, GlobalStyle 전역 설정 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import { ThemeProvider } from "@emotion/react";
 import App from "./App.jsx";
+import theme from "./styles/theme";
+import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>,
 );
