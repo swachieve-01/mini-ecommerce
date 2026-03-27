4515fa5bf5c54c1f8b4381449782b2b8f29fe9b5
ChanWoo Han <brad64405@gmail.com>
2026-03-27 16:38:46 +0900

    feat: 메인 수정

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,16 @@ import { ThemeProvider } from "@emotion/react";
 import App from "./App.jsx";
 import theme from "./styles/theme";
 import GlobalStyle from "./styles/GlobalStyle";
+import Toast from "./components/feedback/Toast.jsx";
 
 createRoot(document.getElementById("root")).render(
-	<StrictMode>
-		<ThemeProvider theme={theme}>
-			<BrowserRouter>
-				<GlobalStyle />
-				<App />
-			</BrowserRouter>
-		</ThemeProvider>
-	</StrictMode>
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <GlobalStyle />
+        <Toast />
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>,
 );
