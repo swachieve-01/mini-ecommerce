5f003a29d45c482ac669b9cd109919b8700078dc
kimsinjung189 <kimsinjung189@gamail.com>
2026-03-29 10:41:40 +0900

    feat : 메인 배너(문구) 추가

---
+++ src/components/Banner/HeroBannerText.jsx
@@ -0,0 +1,45 @@
+// 배너 문구 텍스트
+export const heroBannerMeta = {
+  1: {
+    title: "피부가 편안해지는 순간\n순수 자연 세럼",
+    sub: "오늘, 피부가 달라집니다",
+    position: { top: "50%", left: "15%" },
+    align: "center",
+    hasButton: true,
+    isWhiteButton: false,
+  },
+  2: {
+    title: "자연의 힘으로\n느끼는 깊은 휴식",
+    sub: "피부에 편안한 자연 보습케어",
+    position: { top: "50%", right: "25%" },
+    align: "left",
+    hasButton: false,
+    isWhiteText: true,
+  },
+  3: {
+    title: "단 한 방울로\n완벽히 달라지는 피부",
+    sub: "다양한 친환경 앰플 지금 바로 확인하세요",
+    position: { top: "50%", right: "10%" },
+    align: "center",
+    hasButton: true,
+    isWhiteButton: false,
+  },
+};
+
+// 이벤트 문구
+export const eventBannerMeta = {
+  1: {
+    title: "봄맞이 특별 할인",
+    sub: "최대 50% 세일 진행 중",
+    position: { top: "50%", right: "10%" },
+    align: "left",
+    isWhiteText: true,
+  },
+  2: {
+    title: "신규 회원 혜택",
+    sub: "지금 가입하면 10% 할인 쿠폰",
+    position: { top: "50%", left: "10%" },
+    align: "right",
+    isWhiteText: false,
+  },
+};
