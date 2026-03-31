45fbb5d4114bf2830ddece9c96bf1e7a26f2d984
kimsinjung189 <kimsinjung189@gamail.com>
2026-03-31 11:57:43 +0900

    style : 반응형 추가

--- src/sections/HeroBannerSection.jsx
+++ src/sections/HeroBannerSection.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { heroBannerMeta } from "../components/Banner/HeroBannerText";
+import { heroBannerMeta } from "../data/banners";
 import { getMainBanner } from "../api/Banner";
 import { BannerImage, BannerWrapper } from "../styles/BannerStyle";
 import styled from "@emotion/styled";
@@ -7,43 +7,59 @@ import styled from "@emotion/styled";
 // 이미지 위 텍스트 박스
 const TextBox = styled.div`
   position: absolute;
-
   top: ${({ position }) => position?.top || "50%"};
   left: ${({ position }) => position?.left};
   right: ${({ position }) => position?.right};
 
-  transform: ${({ position }) =>
-    position?.left === "50%" ? "translate(-50%, -50%)" : "translateY(-50%)"};
+  transform: translateY(-50%);
 
   text-align: ${({ align }) => align || "center"};
-
   z-index: 2;
+
+  @media (max-width: 768px) {
+    width: 90%;
+    left: 50%;
+    right: auto;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    width: 90%;
+  }
 `;
 
 // 문구
 const Title = styled.h2`
-  font-size: 65px;
-  font-weight: 500;
-  margin-bottom: 25px;
   white-space: pre-line;
+  margin-bottom: 25px;
   color: ${({ isWhiteText }) => (isWhiteText ? "white" : "#4f6a4e")};
+  font-size: clamp(24px, 3.5vw, 60px);
+  font-weight: 500;
+  line-height: 1.3;
 `;
 
 const SubText = styled.p`
-  font-size: 30px;
+  font-size: clamp(14px, 2.5vw, 30px);
   white-space: pre-line;
   color: ${({ isWhiteText }) => (isWhiteText ? "white" : "#4f6a4e")};
+
+  @media (max-width: 1024px) {
+    font-size: 22px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+  }
 `;
 
 // 배너 버튼
 const BannerButton = styled.button`
   color: ${({ isWhite }) => (isWhite ? "#4f6a4e" : "white")};
   background: ${({ isWhite }) => (isWhite ? "white" : "#8FA77E")};
-  font-size: 20px;
+  font-size: clamp(12px, 1.5vw, 20px);
   border-radius: 50px;
   /* border: 2px solid #4f6a4e; */
   margin-top: 45px;
-  padding: 15px 60px;
+  padding: clamp(8px, 1vw, 15px) clamp(20px, 3vw, 60px);
+  transition: all 0.3s ease;
 
   cursor: pointer;
 `;
@@ -91,7 +107,7 @@ export default function HeroSection() {
   if (!banner) return null;
 
   return (
-    <BannerWrapper height="600px">
+    <BannerWrapper height="clamp(300px, 50vw, 600px)">
       <BannerImage src={banner.src} />
 
       <TextBox position={meta.position} align={meta.align}>
