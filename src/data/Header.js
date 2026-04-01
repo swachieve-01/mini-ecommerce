5f6b048857a7385d3947dd9620acdfb66f5dd1ec
kimsinjung189 <kimsinjung189@gamail.com>
2026-04-01 12:40:54 +0900

    refactor: 헤더 구조 개선 및 emotion 전환, 로그인/로그아웃 기능 추가

---
+++ src/data/Header.js
@@ -0,0 +1,68 @@
+export const categoryData = [
+  {
+    id: 1,
+    name: "스킨케어",
+    path: "/category/skincare",
+    subCategories: [
+      { name: "토너", path: "/category/skincare/toner" },
+      { name: "세럼", path: "/category/skincare/serum" },
+      { name: "크림", path: "/category/skincare/cream" },
+      { name: "마스크팩", path: "/category/skincare/maskpack" },
+      { name: "선케어", path: "/category/skincare/suncare" },
+    ],
+  },
+  {
+    id: 2,
+    name: "메이크업",
+    path: "/category/makeup",
+    subCategories: [
+      { name: "쿠션", path: "/category/makeup/cushion" },
+      { name: "파운데이션", path: "/category/makeup/foundation" },
+      { name: "립", path: "/category/makeup/lip" },
+      { name: "아이섀도우", path: "/category/makeup/eyeshadow" },
+      { name: "치크", path: "/category/makeup/cheek" },
+    ],
+  },
+  {
+    id: 3,
+    name: "클렌저",
+    path: "/category/cleanser",
+    subCategories: [
+      { name: "오일", path: "/category/cleanser/oil" },
+      { name: "밤", path: "/category/cleanser/balm" },
+      { name: "워터", path: "/category/cleanser/water" },
+      { name: "폼", path: "/category/cleanser/foam" },
+    ],
+  },
+  {
+    id: 4,
+    name: "헤어·바디 케어",
+    path: "/category/care",
+    subCategories: [
+      { name: "샴푸", path: "/category/care/shampoo" },
+      { name: "트리트먼트", path: "/category/care/treatment" },
+      { name: "헤어오일", path: "/category/care/hairoil" },
+      { name: "두피케어", path: "/category/care/scalp" },
+      { name: "바디워시", path: "/category/care/bodywash" },
+      { name: "바디로션", path: "/category/care/bodylotion" },
+      { name: "핸드크림", path: "/category/care/hand" },
+    ],
+  },
+  {
+    id: 5,
+    name: "향수",
+    path: "/category/perfume",
+    subCategories: [{ name: "퍼퓸", path: "/category/perfume/oddperfume" }],
+  },
+  {
+    id: 6,
+    name: "뷰티툴",
+    path: "/category/beautytool",
+    subCategories: [
+      { name: "퍼프", path: "/category/beautytool/puff" },
+      { name: "브러쉬", path: "/category/beautytool/brush" },
+      { name: "괄사", path: "/category/beautytool/massage" },
+      { name: "파우치", path: "/category/beautytool/pouch" },
+    ],
+  },
+];
