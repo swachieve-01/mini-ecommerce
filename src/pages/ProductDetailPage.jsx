d5315fe159df120ec753b6a85d75c1b912b204de
ChanWoo Han <brad64405@gmail.com>
2026-04-03 10:40:47 +0900

    feat : 장바구니 목록 상세 설명 추가

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -154,6 +154,7 @@ export default function ProductDetailPage() {
       return;
     }
 
+    console.log("현제 상품 데이터 : ", product);
     addToCart({
       id: product.id,
       name: product.name,
@@ -162,6 +163,7 @@ export default function ProductDetailPage() {
       image: selectedImage || thumbImages[0] || "",
       option: "기본 옵션",
       quantity,
+      category: product.categoryId,
     });
 
     showToast(`${product.name} ${quantity}개가 장바구니에 담겼습니다! 🛒`);
